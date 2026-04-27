82b413c02c216ffa8053b9c880af99a9ec893baa
Sebastian Benzian Olsson <sebastian@distinct.se>
2026-04-27 14:16:03 +0200

    Whitelist Hetzner MinIO + sslip hosts for next/image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [],
+    remotePatterns: [
+      // Hetzner MinIO proxy
+      { protocol: 'http', hostname: '46.224.200.254', port: '8090', pathname: '/blob/**' },
+      { protocol: 'http', hostname: '46.224.200.254', port: '9000', pathname: '/**' },
+      { protocol: 'http', hostname: '**.46.224.200.254.sslip.io', pathname: '/**' },
+      { protocol: 'https', hostname: '**.46.224.200.254.sslip.io', pathname: '/**' },
+      { protocol: 'https', hostname: 'singureality.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
+    ],
   },
 }
 
